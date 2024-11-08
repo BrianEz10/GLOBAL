c57bc4e420f15ad0363977a21e1010e0202384bd
BrianEz10 <pabrianlucas@gmail.com>
2024-11-08 18:07:51 -0300

    Clase Detector y Mutador realizadas

--- GLOBAL.py
+++ GLOBAL.py
@@ -2,34 +2,41 @@ class Detector:
     def __init__(self,ADN,detector):
         self.ADN = ADN
         self.detector = detector
-
+        self.base = []
+        self.posicion_mutador = ""
 
     def detectar_mutante(self):
         mutacion_horizontal = self.detector_horizontal()
         mutacion_vertical = self.detector_vertical()
+        mutacion_diagonal = self.detector_diagonal()
 
-        if mutacion_horizontal and mutacion_vertical:
-            print("Hemos encontrado mutaciones tanto horizontales y verticales!")
+        if mutacion_horizontal and mutacion_vertical and mutacion_diagonal:
+            print("Hemos encontrado mutaciones tanto horizontales, verticales y Diagonales!")
         elif mutacion_horizontal:
-            print("Se detecto una mutacion Horizontal! \nNo se encontraron mutaciones verticales")
+            print("Se detecto una mutacion Horizontal! \nNo se encontraron mutaciones verticales ni diagonales")
         elif mutacion_vertical:
-            print("Se detecto una mutacion vertical! \nNo se encontraron mutacion Horizontales")
+            print("Se detecto una mutacion vertical! \nNo se encontraron mutacion Horizontales ni diagonales")
+        elif mutacion_diagonal:
+            print("Se encontro una mutacion Diagonal! \nNo se encontraron mutaciones horizontales ni verticales")
         else:
             print("No se encontro ningun tipo de Mutacion")
         
 
 
     def detector_horizontal(self):
+
         for fila in self.ADN:
             contador_consecutivo = 1 #El contador empiezaen uno por la primer letra
             for i in range(1, len(fila)):
                 if fila[i] == fila[i-1]: #Vemos si lo numeros son iguales para sumarle 1 a contador_consecutivo, si llega a 4 automaticamente lanza un aviso de que se encontro una mutacion
                     contador_consecutivo += 1
                     if contador_consecutivo == self.detector:
-                        print(f"Se detectaron 4 '{fila[i]}' consecutivos en la fila: {fila}")
+                        self.base = [fila[i],fila[i],fila[i],fila[i]]
+                        self.posicion_mutador = "Horizontal"
                         return True
                 else:
                     contador_consecutivo = 1 #En caso de que no sean iguales las letras se reinicia contador a 0
+
         return False
     
     def detector_vertical(self):
@@ -40,11 +47,49 @@ class Detector:
                 if self.ADN[fila][columnas] == self.ADN[fila-1][columnas]:
                     contador_consecutivo += 1
                     if contador_consecutivo == self.detector:
-                        print(f"Se Encontraron {self.detector} {self.ADN[fila][columnas]} consecutivas en la columna: {columnas+1}")
+                        self.base = [self.ADN[fila][columnas]] * self.detector
+                        self.posicion_mutador = "Vertical"
                         return True
                 else:
                     contador_consecutivo = 0
-        return False
+        return 
+    
+    def detector_diagonal(self):
+        numero_filas = len(self.ADN)
+        numero_columnas = len(self.ADN[0])
+
+        for fila in range(numero_filas - 3): 
+            for columna in range(numero_columnas - 3):
+                contador = 1 
+                for numerito in range(1, 4):
+                    if self.ADN[fila + numerito][columna + numerito] == self.ADN[fila + numerito - 1][columna + numerito - 1]:
+                        contador += 1
+                        if contador == self.detector:
+                            self.base = [self.ADN[fila + numerito][columna + numerito]] * self.detector 
+                            self.posicion_mutador = "Diagonal"
+                            return True
+                    else:
+                        contador = 0
+
+
+
+class Mutador:
+    def __init__(self, base_nitrogenada):
+        self.base_nitrogenada = base_nitrogenada
+
+
+    def crear_mutante(self):
+        pass
+
 
 ADN = Detector(["AGATCA", "GATTCA", "CAATAT", "GAGTTA", "ATTGCG", "CTGTTC"], 4)
 ADN.detectar_mutante()
+base_nitrogenada =  []
+posicion_de_mutacion = ""
+posicion_de_mutacion = ADN.posicion_mutador
+if ADN.base:
+    base_nitrogenada.append(ADN.base)
+print(base_nitrogenada)
+print(posicion_de_mutacion)
+
+coso = Mutador(base_nitrogenada,)
